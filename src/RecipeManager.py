975d9ea63b0ac8d8c1276b87d217972689b770df
AdrianTheHacker <57376726+AdrianTheHacker@users.noreply.github.com>
2025-04-22 13:47:36 -0400

    Adding Vectorizing Function

--- src/RecipeManager.py
+++ src/RecipeManager.py
@@ -2,7 +2,7 @@ import Levenshtein
 
 
 class RecipeManager:
-  def matchRecipe(self, userInputtedRecipeName: str, recipesList: list[str], englishWordsList: list[str]) -> str:
+  def matchRecipe(self, userInputtedRecipeName: str, recipesList: list[str]) -> str:
     """
     Returns the string most similar to `userInputtedRecipeName` in `recipesList`.
 
@@ -12,6 +12,8 @@ class RecipeManager:
     - Not including the missing word, each word in `userInputtedRecipeName`
     will have an Levenshtein distance less than or equal to 3 edits.
     - All string data will be provided in lowercase only.
+    - You may use the `Levenshtein.distance(s1, s2)` from https://rapidfuzz.github.io/Levenshtein/levenshtein.html#distance 
+    for calculating Levenshtein Distance.
     - If the input is blank, return `"None"`.
     - If multiple strings are equally similar, return a string containing all
     answers separated by `, ` in alphabetical order.
@@ -19,7 +21,7 @@ class RecipeManager:
     Example:
     .. code-block:: python
       >>> recipeManager.matchRecipe("corean fred chickee", recipesList)
-      soy garlic korean fried chicken, spicy korean fried chicken
+      garlic korean fried chicken, spicy korean fried chicken
 
     Parameters
     ----------
@@ -35,29 +37,103 @@ class RecipeManager:
     str
       The string most similar to `userInputtedRecipeName` in `recipesList`.
     """
-
+ 
     # 1. Use Levenshtein Distance to correct spelling mistakes.
-    userInputtedRecipeNameList = userInputtedRecipeName.split(" ")
+    recipeWords = self.__getWordsList(recipesList)
+    correctlySpelledUserInputtedRecipeNameMatrix = self.__getCorrectSpellingOfPhrase(userInputtedRecipeName, recipeWords)
 
-    for recipeWord, recipeWordIndex in enumerate(userInputtedRecipeNameList):
-      closestWord = "None"
-      closestWordScore = len(recipeWord)
-
-      for englishWord in englishWordsList:
-        if recipeWord == englishWord:
-          closestWord = recipeWord
-          closestWordScore = 0
-          break
-
-        if Levenshtein.distance(recipeWord, englishWord) >= closestWordScore: continue
-        closestWord = englishWord
-        closestWordScore = Levenshtein.distance(recipeWord, englishWord)
-
-      userInputtedRecipeNameList[recipeWordIndex] = closestWord
-    matchedRecipeName = ' '.join(userInputtedRecipeNameList)
-
-
-
+    print(recipeWords)
+    print(self.__getVectorizedPhrase(" ".join(recipeWords), recipeWords))
+    print(self.__getVectorizedPhrase("spicy korean fried chicken", recipeWords))
     # 2. Use Cosine Similarity to find most similar words combination.
+    # Use bag of words method
 
-    return matchedRecipeName
+    return ""
+  
+  def __getVectorizedPhrase(self, phrase: str, referenceWords: list[str]) -> list:
+    vectorizedPhrase = []
+    for word in referenceWords:
+      if phrase.split(" ").count(word) == 0: 
+        vectorizedPhrase.append(0)
+        continue
+
+      vectorizedPhrase.append(1)
+
+    return vectorizedPhrase
+  
+  def __getCorrectSpellingOfPhrase(self, string: str, referenceWords: list[str]) -> list[list[str]]:
+    """
+    Returns a matrix where each row is a word in `string` and each column represents
+    an alternative for correctly spelling it with equal edit distance.
+
+    Parameters
+    ----------
+    string: str
+      A phrase likely containing spelling mistakes
+
+    referenceWords: list[str]
+      A list of words that are used to check spelling in string.
+      Allowing a small sample of words to be referenced rather than
+      the entire English dictionary can be used in order to prevent 
+      words with no relevance to the search from showing up in the data.
+
+    Returns
+    -------
+    list[list[str]]
+      A matrix where each row is a word in `string` and each column represents
+      an alternative for correctly spelling it with equal edit distance.
+    """
+    words = string.split(" ")
+    closestWords: list[list[str]] = []
+
+    for wordIndex, word in enumerate(words):
+      lowestEditDistance = len(word)
+      closestWords.append(["None"])
+
+      for referenceWord in referenceWords:
+        if word == referenceWord:
+          closestWords[wordIndex] = [referenceWord]
+          lowestEditDistance = 0
+          break
+        
+        wordEditDistance = Levenshtein.distance(word, referenceWord)
+
+        if wordEditDistance > lowestEditDistance: continue
+        if wordEditDistance == lowestEditDistance:
+          closestWords[wordIndex].append(referenceWord)
+          continue
+
+        closestWords[wordIndex] = [referenceWord]
+        lowestEditDistance = wordEditDistance
+
+    return closestWords
+
+  def __getWordsList(self, phrases: list[str]) -> list[str]:
+    """
+    Returns a list of all unique words in a list of strings.
+
+    Example:
+    .. code-block:: python
+    >>> samplePhrases = ["cow jumps over moon", "cow eats food", "cow flies to moon"]
+    >>> recipeManager.__getWordsList(samplePhrases)
+    ['cow', 'jumps', 'over', 'moon', 'eats', 'food', 'flies', 'to']
+
+    Parameters
+    ----------
+    phrases: list[str] 
+      A list of phrases that the function will find all 
+      unique words in.
+
+    Returns
+    -------
+    list[str]
+      A list of all unique words found inside of `phrases`.
+    """
+    wordsList = []
+
+    for phrase in phrases:
+      for word in phrase.split(" "):
+        if wordsList.count(word) == 1: continue
+        wordsList.append(word)
+
+    return wordsList
--- src/test_RecipeManager.py
+++ src/test_RecipeManager.py
@@ -11,6 +11,10 @@ class TestRecipeManager(unittest.TestCase):
   1. Corey Schafer - Python Tutorial: Unit Testing Your Code with the unittest Module
   https://youtu.be/6tNS--WetLI?si=8J3gejHBml4sx_GB
   """
+  def setUp(self):
+    self.recipeManager = RecipeManager()
+    self.recipesList = self.__getRecipesList()
+    # self.englishWordsList = self.__getEnglishWordsList()
 
   def __getRecipesList(self) -> list[str]:
     with open("..\\sample-data\\recipes.json", "r") as recipesFile:
@@ -23,21 +27,20 @@ class TestRecipeManager(unittest.TestCase):
 
     return recipeNamesList
   
-  def __getEnglishWordsList(self) -> list[str]:
-    with open("..\\sample-data\\englishWords.json", "r") as englishWordsFile:
-      englishWords = json.load(englishWordsFile)
-    englishWordsFile.close()
+  # def __getEnglishWordsList(self) -> list[str]:
+  #   with open("..\\sample-data\\englishWords.json", "r") as englishWordsFile:
+  #     englishWords = json.load(englishWordsFile)
+  #   englishWordsFile.close()
 
-    englishWordsList: list[str] = list(englishWords.keys())
-    return englishWordsList
+  #   englishWordsList: list[str] = list(englishWords.keys())
+  #   return englishWordsList
 
   def test_matchRecipe(self):
-    recipesList = self.__getRecipesList()
-    englishWordsList = self.__getEnglishWordsList()
+    results0 = self.recipeManager.matchRecipe("Corean Fred Chickee", self.recipesList)
+    self.assertEqual(results0, "korean fried chicken")
 
-    matchRecipeResult = self.recipeManager.matchRecipe("Corean Fred Chickee", recipesList)
-    self.assertEqual(matchRecipeResult, "Soy Garlic Korean Fried Chicken, Spicy Korean Fried Chicken".lower())
-
+    results1 = self.recipeManager.matchRecipe("Munerone Sop", self.recipesList)
+    self.assertEqual(results1, "minestrone soup")
 
 if __name__ == "__main__":
   unittest.main()
